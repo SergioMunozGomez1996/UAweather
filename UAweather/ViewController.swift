cbbfb035cb09a3f12861da58b572595fee5334f3
mastermoviles <smg158@alu.ua.es>
2019-10-24 19:05:54 +0200

    Apartado D

--- UAweather/ViewController.swift
+++ UAweather/ViewController.swift
@@ -23,22 +23,27 @@ class ViewController: UIViewController {
         let url = URL(string:urlString)
         let dataTask = URLSession.shared.dataTask(with: url!) {
             datos, respuesta, error in
-               let jsonStd = try! JSONSerialization.jsonObject(with: datos!, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:AnyObject]
-               let weather = jsonStd["weather"]! as! [AnyObject]
-               let currentWeather = weather[0] as! [String:AnyObject]
-               let descripcion = currentWeather["description"]! as! String
-               print("El tiempo en \(localidad) es: \(descripcion)")
-               //Estamos bajándonos la imagen pero todavía no la usamos
-               let icono = currentWeather["icon"]! as! String
-               if let urlIcono = URL(string: self.OW_URL_BASE_ICON+icono+".png" ) {
-                  let datosIcono = try! Data(contentsOf: urlIcono)
-                  let imagenIcono = UIImage(data: datosIcono)
-                OperationQueue.main.addOperation() {
-                    self.tiempoImagen.image=imagenIcono
-                    self.tiempoLabel.text=descripcion
-                    UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            do{
+                let jsonStd = try JSONSerialization.jsonObject(with: datos!, options: JSONSerialization.ReadingOptions.mutableContainers) as! [String:AnyObject]
+                let weather = jsonStd["weather"]! as! [AnyObject]
+                let currentWeather = weather[0] as! [String:AnyObject]
+                let descripcion = currentWeather["description"]! as! String
+                print("El tiempo en \(localidad) es: \(descripcion)")
+                //Estamos bajándonos la imagen pero todavía no la usamos
+                let icono = currentWeather["icon"]! as! String
+                if let urlIcono = URL(string: self.OW_URL_BASE_ICON+icono+".png" ) {
+                   let datosIcono = try! Data(contentsOf: urlIcono)
+                   let imagenIcono = UIImage(data: datosIcono)
+                 OperationQueue.main.addOperation() {
+                     self.tiempoImagen.image=imagenIcono
+                     self.tiempoLabel.text=descripcion
+                     UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                 }
                 }
-               }
+            }catch{
+                
+            }
+               
     }
         OperationQueue.main.addOperation() {
             UIApplication.shared.isNetworkActivityIndicatorVisible = true
